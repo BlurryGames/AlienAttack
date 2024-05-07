d1649a2d404de8eaeef3af1ac2ed0890d61a9311
Blurry <julian.lb.games@gmail.com>
2024-05-07 21:00:30 +0200

    Lesson12: Spawn Enemy Function

--- Scripts/EnemySpawner.gd
+++ Scripts/EnemySpawner.gd
@@ -1,4 +1,16 @@
 class_name EnemySpawner extends Node2D
 
-func _on_timer_timeout():
-	pass
+@onready var spawnPositions: Node2D = $SpawnPositions
+
+var enemyScene: PackedScene = preload("res://Scenes/enemy.tscn")
+
+func _on_timer_timeout()-> void:
+	spawnEnemy()
+
+func spawnEnemy()-> void:
+	var spawnPositionsArray: Array[Node] = spawnPositions.get_children()
+	var randomSpawnPosition: Marker2D = spawnPositionsArray.pick_random()
+	
+	var enemyInstance: Area2D = enemyScene.instantiate()
+	enemyInstance.global_position = randomSpawnPosition.global_position
+	add_child(enemyInstance)
