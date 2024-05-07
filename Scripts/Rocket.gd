345fe9429a34dfe82e942fbb2f5a5c5d1f07ec1e
Blurry <julian.lb.games@gmail.com>
2024-05-07 20:18:08 +0200

    Lesson9: Deleting Unwanted Rockets

--- Scripts/Rocket.gd
+++ Scripts/Rocket.gd
@@ -1,6 +1,14 @@
 class_name Rocket extends Area2D
 
+@onready var visibleNotifier: VisibleOnScreenNotifier2D = $VisibleNotifier2D
+
 @export var speed: float = 300.0
+
+func _ready()-> void:
+	visibleNotifier.screen_exited.connect(_on_screen_exited)
 
 func _physics_process(delta: float)-> void:
 	global_position.x += speed * delta
+
+func _on_screen_exited()-> void:
+	queue_free()
