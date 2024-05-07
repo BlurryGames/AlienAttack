74cbe4daa078d30d9db65c058d80c46eefb693ad
Blurry <julian.lb.games@gmail.com>
2024-05-07 20:08:01 +0200

    Lesson8: Creating References To Nodes

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,5 +1,7 @@
 class_name Player extends CharacterBody2D
 
+@onready var rocketContainer: Node = $RocketContainer
+
 var rocketScene: PackedScene = preload("res://Scenes/rocket.tscn")
 var speed: float = 300.0
 
@@ -26,5 +28,6 @@ func _physics_process(_delta: float)-> void:
 
 func shoot()-> void:
 	var rocketInstance: Area2D = rocketScene.instantiate()
-	add_child(rocketInstance)
+	rocketContainer.add_child(rocketInstance)
+	rocketInstance.global_position = global_position
 	rocketInstance.global_position.x += 80.0
