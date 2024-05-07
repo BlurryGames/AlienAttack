6041cf21ee7bc5fc77fd6d2bfc77a846cfad5e57
Blurry <julian.lb.games@gmail.com>
2024-05-07 18:19:32 +0200

    Lesson3: Player Movement

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,1 +1,15 @@
 class_name Player extends CharacterBody2D
+
+var speed: float = 300.0
+
+func _physics_process(delta: float)-> void:
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("MoveRight"):
+		velocity.x = speed
+	if Input.is_action_pressed("MoveLeft"):
+		velocity.x = -speed
+	if Input.is_action_pressed("MoveUp"):
+		velocity.y = -speed
+	if Input.is_action_pressed("MoveDown"):
+		velocity.y = speed
+	move_and_slide()
