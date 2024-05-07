12fbfeb34ff904a77f5784763d6bd545021f0d62
Blurry <julian.lb.games@gmail.com>
2024-05-07 18:57:48 +0200

    Lesson6: Understanding Delta Time

--- Scripts/Rocket.gd
+++ Scripts/Rocket.gd
@@ -1,6 +1,6 @@
 class_name Rocket extends Area2D
 
-@export var speed: float = 5.0
+@export var speed: float = 300.0
 
-func _physics_process(_delta: float)-> void:
-	global_position.x += speed
+func _physics_process(delta: float)-> void:
+	global_position.x += speed * delta
